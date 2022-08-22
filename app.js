b0000c5615dc54dd3f7a3a2c54543a149cf73a38
Selim Aktay <sellimakty99@gmail.com>
2022-08-22 14:26:59 +0300

    staticFiles1

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-const fn = function () {
-  console.log('test 1');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const myLogger = (req, res, next) => {
+  console.log('test1');
+  next();
 };
+
+//middlewares
+app.use(express.static('public'));
+app.use(myLogger);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`sunucu ${port} portunda başlatildi...`);
+});
