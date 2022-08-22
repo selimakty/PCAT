cdb4fac048b86e20e814b1b9ca621e26bae37702
Selim Aktay <sellimakty99@gmail.com>
2022-08-22 16:11:28 +0300

    ejs done

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
 
-const myLogger = (req, res, next) => {
-  console.log('test1');
-  next();
-};
+//template engine
+app.set('view engine', 'ejs');
 
 //middlewares
 app.use(express.static('public'));
-app.use(myLogger);
 
+//routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
 });
+app.get('/about', (req, res) => {
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('add');
+});
+app.get('/photo', (req, res) => {
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('photo');
+});
+
+
+
 
 const port = 3000;
 
